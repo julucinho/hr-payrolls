fce337dc77988ba8233fe0c7d9e95107b2ffee99
Zé Lúcio <joselucioalmeidajunior@gmail.com>
2021-12-21 01:49:21 -0300

    feat: starting project

---
+++ src/main/java/com/julucin/hrpayrolls/HrPayrollsApplication.java
@@ -0,0 +1,13 @@
+package com.julucin.hrpayrolls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayrollsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayrollsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julucin/hrpayrolls/HrPayrollsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julucin.hrpayrolls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
