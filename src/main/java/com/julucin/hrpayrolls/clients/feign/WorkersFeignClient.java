de4a1a2edf70dd32153318ea2b9715c9e1a22ff1
Zé Lúcio <joselucioalmeidajunior@gmail.com>
2021-12-22 06:04:19 -0300

    feat: registering at Eureka Server

--- src/main/java/com/julucin/hrpayrolls/clients/feign/WorkersFeignClient.java
+++ src/main/java/com/julucin/hrpayrolls/clients/feign/WorkersFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name="hr-workers", url = "localhost:8001", path = "/workers")
+@FeignClient(name="hr-workers", path = "/workers")
 public interface WorkersFeignClient {
 
     @GetMapping("/{id}")
